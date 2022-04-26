ba424af4646ff458f0b5de67a90bc290d45b04f4
+ <diazcristina.la@gmail.com>
2022-04-25 23:18:34 -0700

    commit tree 9f92f1c83a92b8de444a9cca54af04f3c80f0d24

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("dance");
+
+function mondayWork(labor = "go to the office") {
+  return `This Monday, I will ${labor}.`;
+}
+saturdayFun("work from home");
+
+function wrapAdjective(deco = "*") {
+  const innerFunction = function (adjective = "special") {
+    return `You are ${deco}${adjective}${deco}!`;
+  };
+  return innerFunction;
+}
+
+wrapAdjective("%")("a dedicated programer");
